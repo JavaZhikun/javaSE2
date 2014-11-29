7d360cff2a9baacdc99b18866bd904f9936fe289
luanzhikun <972663246@qq.com>
2014-11-29 23:29:17 +0800

    项目提交

---
+++ src/lesson4/GenericTest.java
@@ -0,0 +1,10 @@
+package lesson4;
+
+public class GenericTest{
+
+	public static void main(String[] args)
+	{
+		System.out.println("hello world");
+	}
+
+}
---
+++ src/lesson4/test.java
@@ -0,0 +1,10 @@
+package lesson4;
+
+public class test {
+	public static void main(String[] args) {
+		
+		System.out.println("hello world");
+	}
+	
+
+}
